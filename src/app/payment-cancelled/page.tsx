1f5df72b9a0e59273a730c2f2895ee20943f7c7c
jarocode <okolieuchenna890@gmail.com>
2024-06-16 19:09:49 +0100

    payment cancelled and success pages

---
+++ src/app/payment-cancelled/page.tsx
@@ -0,0 +1,40 @@
+"use client";
+
+import React from "react";
+import styled from "@emotion/styled";
+import { Typography, Button } from "@mui/material";
+
+const page = () => {
+  return (
+    <Container>
+      <Typography fontSize="36px">
+        Your subscription has been cancelled!
+      </Typography>
+      <Button
+        size="large"
+        sx={{
+          padding: ".5rem 2.5rem",
+          background: "#fff",
+          color: "#000",
+          "&:hover": {
+            background: "#fff",
+          },
+        }}
+      >
+        Back
+      </Button>
+    </Container>
+  );
+};
+
+export default page;
+
+const Container = styled.div`
+  width: 100%;
+  height: 100vh;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  gap: 2.5rem;
+  align-items: center;
+`;
